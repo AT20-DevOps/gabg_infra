f446d6d52e75bfa6c23855914a0b4920cb73e75b
Giovanni <giometal94@gmail.com>
2023-06-09 20:53:34 -0400

    Add the correct host_interface

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,13 +11,13 @@ resource "virtualbox_vm" "node" {
     count = 1
     name = format("node-%02d", count.index +1)
     image = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
-    cpus = 1
+    cpus = 2
     memory = "512mib"
     user_data = ""
     
     network_adapter{
         type = "hostonly"
-        host_interface="vboxnet1"
+        host_interface="VirtualBox Host-Only Ethernet Adapter"
     }
 }
 
