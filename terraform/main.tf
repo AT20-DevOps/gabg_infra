6e82ffaf1417cfe5a0c47a6a351446aad38501ab
Giovanni <giometal94@gmail.com>
2023-06-12 18:41:35 -0400

    Add provision code with docker install script

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,25 +6,38 @@ terraform {
         }
     }
 }
-
 resource "virtualbox_vm" "node" {
-    count = 1
-    name = format("node-%02d", count.index +1)
-    image = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
-    cpus = 2
-    memory = "512mib"
+    count     = 1
+    name      = format("node-%02d", count.index + 1)
+    image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+    cpus      = 2
+    memory    = "2048 mib"
     user_data = ""
-    
-    network_adapter{
-        type = "hostonly"
-        host_interface="VirtualBox Host-Only Ethernet Adapter"
+
+    network_adapter {
+    type           = "bridged"
+    host_interface = "Intel(R) Wi-Fi 6 AX201 160MHz"
+    }
+    // ssh conncetion
+    connection {
+        type = "ssh"
+        host = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+        user = "vagrant"
+        private_key = file("vagrant")
+    }
+
+    provisioner "file" {
+        source = "../scripts/install_docker.sh"
+        destination = "/tmp/install_docker.sh"
+    }
+
+    // provision
+    provisioner "remote-exec" {
+        inline = [ 
+            "chmod +x /tmp/install_docker.sh",
+            "/tmp/install_docker.sh" ]
     }
 }
-
 output "IPAddr" {
-    value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address,1)
-}
-
-output "IPAddr_2" {
-    value=element(virtualbox_vm.node.*.network_adapter.0.ipv4_address,2)
+    value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
 }
